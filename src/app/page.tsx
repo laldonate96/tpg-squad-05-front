f62b994a5c46bc4d60f2d42d2898e6ed5908b92f
milithome <mthome@fi.uba.ar>
2024-11-23 19:04:00 -0300

    Acomodo el boton que redirecciona al modulo de finanzas y cosas visuales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Navbar />
-      <h1 className="mb-8 text-black font-bold text-2xl">Welcome to TPG Front</h1>
+      <h1 className="mb-8 text-black font-bold text-2xl">Carga de horas WEB</h1>
       <ClientCalendarWrapper />
     </main>
   )
