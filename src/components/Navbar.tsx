00799e1fbae79160319a385fe597a914872945d6
laldonate96 <lucasaldonate@gmail.com>
2024-11-28 11:21:44 -0300

    feat: add link to finance module

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
    <nav className="fixed top-4 right-4 z-10 space-y-2">
      <div className="bg-gray-200 shadow-lg rounded-lg">
        <Link
-         href="/finance-module"
+         href="https://squad11-2024-2c-frontend.onrender.com/"
          className="flex items-center gap-2 px-4 py-2 text-black hover:text-blue-600 transition-colors duration-200"
        >
          <span>Ir a modulo de finanzas</span>
